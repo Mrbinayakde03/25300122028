22a4433b3250421560536c9035d4db04d9e7d56c
Binayak De <mr.binayakde@gmail.com>
2025-07-29 14:24:35 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AppRouter from "./routes/AppRouter";
 import { LoggerProvider } from "./context/LoggerContext";
-// import { CssBaseline } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
